3cbdcc217147c167061593a477054007ea2d23fd
Vivekanandan Ramalingam <vir@DN2lk5fud.stanford.edu>
2021-09-28 22:28:26 -0700

    preprocessing_ts

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,70 +1,184 @@
 #!/bin/bash
 
-# The entry point for the TF-Atlas pipeline
-# 
-# This script assumes that the present working directory is the 
-# git cloned TF-Atlas repo folder
+# TF-Atlas pipeline
+# Step 1. Copy reference files from gcp
+# Step 2. Download bams and peaks file for the experiment
+# Step 3. Process bam files to generate bigWigs
+# Step 4. Modeling, predictions, metrics, shap, modisco, embeddings
+# Step 5. Generate reports
 
+# import the utils script
 source pipeline/utils.sh
 
-# command line params
-experiment=$1
-tuning=$2
-learning_rate=$3
-# use -1 for counts_loss_weight if you want to auto set
-counts_loss_weight=$4 
-epochs=$5
-encode_access_key=$6
-encode_secret_key=$7
-#gbsc-gcp-lab-kundaje-tf-atlas
-gcp_bucket=$8
-pipeline_destination=$9
-metadata_file_path=${10}
 
-# create the log file
-logfile=$PWD/$experiment.log
+
+# path to json file with pipeline params
+pipeline_json=$1
+
+# get params from the pipleine json
+experiment=`jq .experiment $pipeline_json | sed 's/"//g'` 
+
+assembly=`jq .assembly $pipeline_json | sed 's/"//g'`
+
+unfiltered_alignments=`jq .unfiltered_alignments $pipeline_json | sed 's/"//g'`
+
+unfiltered_alignments_md5sums=\
+`jq .unfiltered_alignments_md5sums $pipeline_json | sed 's/"//g'`
+
+alignments=`jq .alignments $pipeline_json | sed 's/"//g'`
+
+alignments_md5sums=`jq .alignments_md5sums $pipeline_json | sed 's/"//g'`
+
+control_unfiltered_alignments=\
+`jq .control_unfiltered_alignments $pipeline_json | sed 's/"//g'`
+
+control_unfiltered_alignments_md5sums=\
+`jq .control_unfiltered_alignments_md5sums $pipeline_json | sed 's/"//g'`
+
+control_alignments=`jq .control_alignments $pipeline_json | sed 's/"//g'`
+
+control_alignments_md5sums=\
+`jq .control_alignments_md5sums $pipeline_json | sed 's/"//g'`
+
+peaks=`jq .peaks $pipeline_json | sed 's/"//g'`
+
+peaks_md5sum=`jq .peaks_md5sum $pipeline_json | sed 's/"//g'`
+
+has_control=`jq .has_control $pipeline_json | sed 's/"//g'`
+
+stranded=`jq .stranded $pipeline_json | sed 's/"//g'`
+
+model_arch_name=`jq .model_arch_name $pipeline_json | sed 's/"//g'`
+
+sequence_generator_name=\
+`jq .sequence_generator_name $pipeline_json | sed 's/"//g'`
+
+splits_json_path=`jq .splits_json_path $pipeline_json | sed 's/"//g'`
+
+test_chroms=`jq .test_chroms $pipeline_json | sed 's/"//g'`
+
+tuning=`jq .tuning $pipeline_json | sed 's/"//g'`
+
+learning_rate=`jq .learning_rate $pipeline_json | sed 's/"//g'`
+
+counts_loss_weight=`jq .counts_loss_weight $pipeline_json | sed 's/"//g'`
+
+epochs=`jq .epochs $pipeline_json | sed 's/"//g'`
+
+gcp_bucket=`jq .gcp_bucket $pipeline_json | sed 's/"//g'`
+
+encode_access_key=$2
+
+encode_secret_key=$3
+
+# the place where the results of the pipeline run will be stored
+# either mnt or gcp. If "mnt" then output files are stored in 
+# --mnt target, or if "gcp" then the files are uploaded to gcp
+pipeline_destination=$4
+
+$reference_file=$5
+$reference_file_index=$6
+
+# create log file
+logfile=$experiment.log
 touch $logfile
 
-# path to the metadata file in the Tf-Atlas folder
-echo $( timestamp ): "metadata_file_path - " $metadata_file_path | \
-tee -a $logfile
 
-# change directory to the pipeline folder
-cd pipeline
 
-# create pipeline params json
-echo $( timestamp ): "
-python \\
-    create_pipeline_params_json.py \\
-    $metadata_file_path \\
-    $experiment \\
-    True \\
-    True \\
-    BPNetd10008 \\
-    BPNet \\
-    one_split.json \\
-    chr1 \\
-    $tuning \\
-    $learning_rate \\
-    $counts_loss_weight \\
-    $epochs \\
-    $gcp_bucket \\
-    params_file.json" | tee -a $logfile
+# Step 0. Create all required directories
+
+dst_dir=$PWD/
+
+# local reference files directory
+reference_dir=${dst_dir}reference
+echo $( timestamp ): "mkdir" $reference_dir | tee -a $logfile
+mkdir $reference_dir
+
+# directory to store downloaded files
+downloads_dir=${dst_dir}downloads
+echo $( timestamp ): "mkdir" $downloads_dir | tee -a $logfile
+mkdir $downloads_dir
+
+# directory to store intermediate preprocessing files
+# (merged bams, bedGraphs)
+intermediates_dir=${dst_dir}intermediates
+echo $( timestamp ): "mkdir" $intermediates_dir | tee -a $logfile
+mkdir $intermediates_dir
+
+# directory to store bigWigs
+bigWigs_dir=${dst_dir}bigWigs
+echo $( timestamp ): "mkdir" $bigWigs_dir | tee -a $logfile
+mkdir $bigWigs_dir
+
+
+# Step 1: Copy the reference file
+
+echo $( timestamp ):cp $reference_file $reference_dir/; cp $reference_file_index $reference_file_index | \\
+tee -a $logfile 
+
+
+cp $reference_file $reference_dir/
+cp $reference_file_index $reference_file_index
+
+# Step 2. download bam files and peaks file
+
+# 2.1 download unfiltered alignments bams
+download_file "$unfiltered_alignments" "bam" \
+"$unfiltered_alignments_md5sums" 1 $logfile $encode_access_key \
+$encode_secret_key $downloads_dir
+
+# 2.2 download alignments bams
+download_file "$alignments" "bam" "$alignments_md5sums" 1 $logfile \
+$encode_access_key $encode_secret_key $downloads_dir
+
+if [ "$has_control" = "True" ]
+then
+    # 2.3 download control unfiltered alignmentsbams
+    download_file "$control_unfiltered_alignments" "bam" \
+    "$control_unfiltered_alignments_md5sums" 1 $logfile $encode_access_key \
+    $encode_secret_key $downloads_dir
+
+    # 2.4 download control alignments bams
+    download_file "$control_alignments" "bam" "$control_alignments_md5sums" 1 \
+    $logfile $encode_access_key $encode_secret_key $downloads_dir
+fi
+
+# 2.5 download peaks file
+download_file $peaks "bed.gz" $peaks_md5sum 1 $logfile $encode_access_key \
+$encode_secret_key $downloads_dir
+
+wait_for_jobs_to_finish "Download"
+
+# Step 3. preprocess
+
+# 3.1 preprocess experiment bams
+./preprocessing.sh $experiment "$unfiltered_alignments" "$alignments" \
+$downloads_dir $intermediates_dir $bigWigs_dir $stranded False $reference_dir \
+$logfile &
+
+echo $( timestamp ): [$!] "./preprocessing.sh" $experiment \
+\"$unfiltered_alignments\" \"$alignments\" $downloads_dir $intermediates_dir \
+$bigWigs_dir $stranded False $reference_dir $logfile  | tee -a $logfile
+
+if [ "$has_control" = "True" ]
+then
+    # 3.2 preprocess experiment control bams
+    ./preprocessing.sh $experiment "$control_unfiltered_alignments" \
+    "$control_alignments" $downloads_dir $intermediates_dir $bigWigs_dir \
+    $stranded True $reference_dir $logfile &
     
-python \
-    create_pipeline_params_json.py \
-    $metadata_file_path \
-    $experiment \
-    True \
-    True \
-    BPNet1000d8 \
-    BPNet \
-    one_split.json \
-    chr1 \
-    $tuning \
-    $learning_rate \
-    $counts_loss_weight \
-    $epochs \
-    $gcp_bucket \
-    params_file.json
+    echo $( timestamp ): [$!] "./preprocessing.sh" $experiment \
+    \"$control_unfiltered_alignments\" \"$control_alignments\" $downloads_dir \
+    $intermediates_dir $bigWigs_dir $stranded True $reference_dir $logfile | \
+    tee -a $logfile
+fi
 
+wait_for_jobs_to_finish "Preprocessing"
+
+# Step pre_4: Create the input json for the experiment that will
+# be used in training
+echo $( timestamp ): "python create_input_json.py" $experiment $peaks True \
+True $bigWigs_dir $downloads_dir . | tee -a $logfile
+
+python create_input_json.py $experiment $peaks True True $bigWigs_dir \
+$downloads_dir .
--- run_preprocess.wdl
+++ run_preprocess.wdl
@@ -13,19 +13,27 @@ task run_preprocess {
 		String gcp_bucket
 		String pipeline_destination
 		File metadata
+		File reference_file
+		File reference_file_index
   	}	
 	command {
+		#create data directories and download scripts
 		cd /; mkdir my_data
 		cd /scratch/
 		git clone https://github.com/viramalingam/tf_atlas_analysis.git
 		cd tf_atlas_analysis
-		chmod 777 run_preprocess.sh
-		echo "run ./run_preprocess.sh"
-		ls
-		echo "metadata path: ${metadata}" 
-		./run_preprocess.sh ${experiment} ${tuning} ${learning_rate} ${counts_loss_weight} ${epochs} ${encode_access_key} ${encode_secret_key} ${gcp_bucket} ${pipeline_destination} ${metadata}
-		ls pipeline
-		cp pipeline/params_file.json /cromwell_root/params_file.json	
+
+		#run the params create script and preprocess script
+		chmod 777 create_params.sh
+		echo "run ./create_params.sh"
+		./create_params.sh ${experiment} ${tuning} ${learning_rate} ${counts_loss_weight} ${epochs} ${encode_access_key} ${encode_secret_key} ${gcp_bucket} ${pipeline_destination} ${metadata}
+		cp pipeline/params_file.json /cromwell_root/params_file.json	#copy the file to the root folder for cromwell to copy
+
+		##preprocessing
+		chmod 777 run_preprocessing.sh
+		echo "run ./run_preprocessing.sh"
+		./run_preprocessing.sh params_file.json ${encode_access_key} ${encode_secret_key} ${pipeline_destination} ${reference_file} ${reference_file_index}
+		
 	}
 	
 	output {
@@ -50,6 +58,8 @@ workflow preprocess {
 		String gcp_bucket
 		String pipeline_destination
 		File metadata
+		File reference_file
+		File reference_file_index
 	}
 
 	call run_preprocess {
@@ -63,7 +73,9 @@ workflow preprocess {
 			encode_secret_key = encode_secret_key,
         		gcp_bucket = gcp_bucket,
 			pipeline_destination = pipeline_destination,
-			metadata = metadata	
+			metadata = metadata,
+			reference_file = reference_file,
+			reference_file_index = reference_file_index	
  	}
 	output {
 		File params_json = run_preprocess.params_json
