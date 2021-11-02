6b6cf6e15f2adec6c6282ccfd1f6bab6c6db9987
Vivekanandan Ramalingam <vir@DN2lk5es4.stanford.edu>
2021-11-02 00:11:10 -0700

    tuning ts

--- tuning.py
+++ tuning.py
@@ -63,7 +63,7 @@ def main():
 	    'learning_rate': hp.uniform('learning_rate', 0.00001, 0.01),
 	    'counts_loss_weight': hp.uniform('counts_loss_weight', 10, 10000),
 	    'filters': hp.uniform('filters', 24, 72),
-	    'num_dilation_layers': hp.uniform('num_dilation_layers', 4, 8)
+	    'num_dilation_layers': hp.choice('num_dilation_layers', {4, 5, 6, 7, 8})
 	}
 
 	def train_model_and_return_model_loss(params):
